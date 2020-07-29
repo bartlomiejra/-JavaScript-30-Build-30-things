809a87e7d3746fa141aed105d4c1743a539761ca
Bart <bartlomiej.ra@gmail.com>
2020-07-29 20:49:38 +0200

    video

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,32 @@
+// get Out Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progresBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipbutton = player.querySelectorAll('[data_skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+// Build out function
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause';
+   video[method]();
+}
+
+
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    console.log('Update the button');
+
+}
+
+    // Hook up the event listener
+    video.addEventListener('click', togglePlay);
+    video.addEventListener('play', updateButton);
+    video.addEventListener('pause', updateButton);
+
+    toggle.addEventListener('click', togglePlay);
+    
