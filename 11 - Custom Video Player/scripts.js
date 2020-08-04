e5a00e7ec73bb0259f13a3b4e2e4acb89a53487e
Bart <bartlomiej.ra@gmail.com>
2020-08-04 13:56:33 +0200

    update

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,11 +2,10 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progresBar = player.querySelector('.progress__filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
-
 
 // Build out function
 function togglePlay(){
@@ -14,31 +13,49 @@ function togglePlay(){
    video[method]();
 }
 
-
-
 function updateButton(){
     const icon = this.paused ? '►' : '❚ ❚';
-    console.log(icon);
-    console.log('Update the button');
-
+   toggle.textContent = icon;
+    // console.log('Update the button');
 }
-
 
 function skip(){
     console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip)
-
-}
+} 
 
 function handleRangeUpdate(){
- console.log(this.value);   
+video[this.name] = this.value;
+//  console.log(this.name);   
+//  console.log(this.value);   
 }
 
+function handleProgress(){
+    const percent = (video.currentTime/video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX /  progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    // console.log(e);
+
+}
     // Hook up the event listener
     video.addEventListener('click', togglePlay);
     video.addEventListener('play', updateButton);
     video.addEventListener('pause', updateButton);
+    video.addEventListener('timeupdate', handleProgress );
 
     toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
-    
+    ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+    progress.addEventListener("click", scrub);
+
+
+    let mousedown = false;
+    progress.addEventListener('click', scrub);
+    progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+    progress.addEventListener ('mousedown', () => mousedown = true)
+    progress.addEventListener ('mouseup', () => mousedown = false)
+     
